a6d5284f077f13d8552b295cd3c43c2eacc3ac59
mauricechouam <lansichouamou@gmail.com>
2020-08-25 18:40:10 -0700

    pushed new branch

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# React-Portfolio
+# React-portfolio
+
+A boilrplate react portfolio to showcase your projects and work
+
+
+Dummy-portfolio link -> https://shloksomani.github.io/react-portfolio/
+
+![](test.gif)
--- src/index.js
+++ src/index.js
@@ -1,19 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-//adding my imports
-import Navbar from "./component/navbar.js";
-import Footer from "./component/footer.js";
+
+//import css in order
+import 'normalize.css';
+import './animate.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import "./styles/index.scss";
+import './img/icons/css/ionicons.css';
+import './img/font-awesome/css/font-awesome.css';
+import 'lightbox2/dist/css/lightbox.min.css'
+import './style.css';
+
+//import js libraries
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './libs/easing.js';
+import 'lightbox2/dist/js/lightbox.min.js';
+
+import * as serviceWorker from './serviceWorker';
+
+//import components
+import Navbar from './components/navbar.js';
+import Intro from './components/intro.js';
+import About from './components/about.js';
+import Portfolio from './components/portfolio.js';
+import Contact from './components/contact.js';
+import BackToTop from './components/back-top.js';
+import Preloader from './components/preloader.js';
+
+
 
 ReactDOM.render(
-	<React.Fragment>
-		<Navbar />
-		<Footer />
-	</React.Fragment>, document.getElementById('root'));
-
+    <React.Fragment>
+        <Navbar />
+        <Intro />
+        <About />
+        <Portfolio />
+        <Contact />
+        <BackToTop />
+        <Preloader />
+    </React.Fragment>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
